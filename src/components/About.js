31e709ee6d4a65f9d1f81ff61271d0ee04eb6186
duncanlaidlaw1 <slamdunc123@gmail.com>
2020-09-23 22:31:08 +0100

    create components for sections

---
+++ src/components/About.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<section className='resume-section' id='about'>
+			<div className='resume-section-content'>
+				<h1 className='mb-0'>
+					Duncan
+					<span className='text-primary'>Laidlaw</span>
+				</h1>
+				<div className='subheading mb-5'>
+					+44 7968903859&nbsp;·&nbsp;
+					<a href='mailto:name@email.com'>
+						duncan.laidlaw@btinternet.com
+					</a>
+				</div>
+				<p className='lead mb-5'>
+					I am experienced in leveraging agile frameworks to provide a
+					robust synopsis for high level overviews. Iterative
+					approaches to corporate strategy foster collaborative
+					thinking to further the overall value proposition.
+				</p>
+				<div className='social-icons'>
+					<a className='social-icon' href='#'>
+						<i className='fab fa-linkedin-in'></i>
+					</a>
+					<a className='social-icon' href='#'>
+						<i className='fab fa-github'></i>
+					</a>
+					<a className='social-icon' href='#'>
+						<i className='fab fa-twitter'></i>
+					</a>
+					<a className='social-icon' href='#'>
+						<i className='fab fa-facebook-f'></i>
+					</a>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default About;
